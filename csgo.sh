35025aa0d3b660b7f392e167cc70c46c7d2060bb
Jackzmc <me@jackz.me>
2020-02-03 17:04:28 +0000

    fixed start failure

--- csgo.sh
+++ csgo.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd $HOME/hlserver
+~/hlserver/steamcmd.sh +runscript ~/hlserver/csgo_ds.txt &&
 csgo/srcds_run -game csgo -tickrate 128 -autoupdate -steam_dir ~/hlserver -steamcmd_script ~/hlserver/csgo_ds.txt $@
