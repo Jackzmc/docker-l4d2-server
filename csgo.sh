2d1a3a556cd9cc2401a8dc8bae120e9f1f7fd4e4
Jackzmc <me@jackz.me>
2020-02-03 17:15:42 +0000

    removed validate from autorestart update

--- csgo.sh
+++ csgo.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd $HOME/hlserver
-~/hlserver/steamcmd.sh +runscript ~/hlserver/csgo_ds.txt &&
+~/hlserver/steamcmd.sh +login anonymous +force_install_dir ./csgo +app_update 740 validate +quit&&
 csgo/srcds_run -game csgo -tickrate 128 -autoupdate -steam_dir ~/hlserver -steamcmd_script ~/hlserver/csgo_ds.txt $@
